b2e8e957b20a86a0db2be444396dd74d5a0cd3fc
sejesila <silseje5@gmail.com>
2026-04-26 20:28:11 +0300

    Fix deploy script working directory

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -1,28 +1,14 @@
 #!/bin/bash
 set -e
 
+cd /home/farmpedia-finance/htdocs/finance.farmpedia.org  # Add this line
+
 echo "Deployment started ..."
-
-# Enter maintenance mode or return true
-# if already is in maintenance mode
 (php artisan down) || true
-
-# Pull the latest version of the app
-git fetch origin && git reset --hard origin/main
-
-# Install composer dependencies
+git pull origin main
 composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader
-
-# Clear the old cache
 php artisan clear-compiled
-
-# Compile npm assets
 npm run build
-
-# Run database migrations
 php artisan migrate --force
-
-# Exit maintenance mode
 php artisan up
-
 echo "Deployment finished!"
