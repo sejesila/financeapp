99394a0a4fe00ad8b48fd7c48015d6164a9fec7f
sejesila <silseje5@gmail.com>
2026-05-04 13:47:03 +0300

    Email cron job

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,20 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('reports:send-annual')
+    ->hourly()
+    ->withoutOverlapping()
+    ->onSuccess(fn() => \Log::info('Annual reports sent successfully'))
+    ->onFailure(fn() => \Log::error('Annual reports failed'));
+
+Schedule::command('reports:send-monthly')
+    ->hourly()
+    ->withoutOverlapping()
+    ->onSuccess(fn() => \Log::info('Monthly reports sent successfully'))
+    ->onFailure(fn() => \Log::error('Monthly reports failed'));
