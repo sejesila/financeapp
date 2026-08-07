e25e5433478aeadb4c66e0bbb0a87ecee7219280
sejesila <silseje5@gmail.com>
2026-08-07 11:57:28 +0300

    Referrer show fix

--- resources/views/referrer-payouts/show.blade.php
+++ resources/views/referrer-payouts/show.blade.php
@@ -29,7 +29,7 @@
                                class="inline-flex items-center px-4 py-2 bg-indigo-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-indigo-700 focus:bg-indigo-700 active:bg-indigo-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
                                 Pay Out Referrer
                             </a>
-                            <a href="{{ route('loans-given.show') }}"
+                            <a href="{{ route('loans-given.index', ['filter' => 'paid']) }}"
                                class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">
                                 Back
                             </a>
