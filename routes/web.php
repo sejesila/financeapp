f84cfd3fb3803b22d1641c5610d70ca2f2b88b3a
sejesila <silseje5@gmail.com>
2026-09-09 16:56:25 +0300

    Notes edit

--- routes/web.php
+++ routes/web.php
@@ -158,7 +158,7 @@ Route::middleware('auth')->group(function () {
         Route::put('/{loanGiven}/status', [LoanGivenController::class, 'markStatus'])->name('status');
         Route::delete('/{loanGiven}', [LoanGivenController::class, 'destroy'])->name('destroy');
         Route::put('/{loanGiven}/notes', [LoanGivenController::class, 'updateNotes'])
-            ->name('loans-given.notes.update');
+            ->name('notes.update');
     });
 
     Route::get('referrers/{referrer}/payouts/create', [ReferrerPayoutController::class, 'create'])
